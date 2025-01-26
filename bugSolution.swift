f3b60a5f2974288f1f23702e6aa483e1487e21f1
BugHunter-X <askzenai@gmail.com>
2025-01-26 15:44:04 +0100

    Add bugSolution.swift file

---
+++ bugSolution.swift
@@ -0,0 +1,5 @@
+func calculateAverageFixed(numbers: [Double]) -> Double {
+    guard !numbers.isEmpty else { return 0 } 
+    let sum = numbers.reduce(0, +)
+    return sum / Double(numbers.count) 
+}
